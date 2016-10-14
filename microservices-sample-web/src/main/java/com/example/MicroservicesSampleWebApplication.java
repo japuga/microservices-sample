ff339f8d24827a9abe6c7864b33b6de75010c576
japuga <javier_puente@hotmail.com>
2016-10-14 16:05:34 -0500

    Initial revision

---
+++ microservices-sample-web/src/main/java/com/example/MicroservicesSampleWebApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesSampleWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesSampleWebApplication.class, args);
+	}
+}
